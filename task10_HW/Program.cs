c03255fe955cc5b626a0087d5322110dced1303c
Svetlana <s.solodunova@gmail.com>
2023-07-02 01:19:35 +0300

    Добавила ДЗ по 2-ому семинару

---
+++ task10_HW/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int numberTwo = 0;
+
+if (number < 100 || number > 999) Console.WriteLine("Необходимо ввести трехзначное число");
+
+else 
+{
+    numberTwo = number / 10 % 10;
+    Console.WriteLine($"Введенное число - {number}");
+    Console.WriteLine($"Вторая цифра числа - {numberTwo}");
+}
---
+++ task13_HW/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int ThirdNum(int number)
+{
+    while (number > 999)
+    {
+        number = number /10;
+    }
+    return number % 10;
+}
+bool check(int number)
+{
+    if (number < 100) 
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+int number = Prompt("Введите число ");
+if (check(number))  
+{
+    Console.WriteLine($"Третьей цифрой числа {number} является {ThirdNum(number)}");
+}
