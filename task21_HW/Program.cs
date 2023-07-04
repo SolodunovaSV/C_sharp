00f9cbdd4cd0579567bff9cdeb372e389316e8ed
Svetlana <s.solodunova@gmail.com>
2023-07-05 02:04:26 +0300

    Добавила решение ДЗ для 3-го семинара

---
+++ task21_HW/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.WriteLine("Введите координату X для первой точки ");
+int aX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y для первой точки ");
+int aY = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z для первой точки ");
+int aZ = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату X для второй точки ");
+int bX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y для второй точки ");
+int bY = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z для второй точки ");
+int bZ = Convert.ToInt32(Console.ReadLine());
+
+int a = aY - bY;
+int b = aX - bX;
+int c = aZ - bZ;
+double distance = Math.Sqrt(a * a + b * b + c * c);
+Console.WriteLine(distance);
---
+++ task23_HW/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int mult = 0;
+
+if (Number > 0)
+{
+    while (count < Number + 1)
+    {
+        mult = count * count * count;
+        count++;
+        Console.Write($"{mult}, ");
+    }
+}
+else 
+{
+    count = count * (-1);
+    while (count > Number - 1)
+    {
+        mult = (count * count * count) * (-1);
+        count = count - 1;
+        Console.Write($"{mult}, ");
+    }
+}
