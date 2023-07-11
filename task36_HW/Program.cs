ffaae2c2191bc3bc95c50d06dab9a1814c5c3d55
Svetlana <s.solodunova@gmail.com>
2023-07-11 23:49:51 +0300

    Добавила решение ДЗ для 5-го семинара

---
+++ task36_HW/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArray(numbers);
+PrintArray(numbers);
+
+int sum = 0;
+
+for (int i = 1; i < numbers.Length; i += 2)
+{
+    sum += numbers[i];
+}
+
+void FillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1, 100);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+}
+
+Console.WriteLine();
+Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях = {sum}");
