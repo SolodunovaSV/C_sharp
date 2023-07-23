67b9b9bf41c95df9e7a8c0a353aa6532f6e0d819
Svetlana <s.solodunova@gmail.com>
2023-07-23 21:31:26 +0300

    Добавла задания, выполненные на 8 семинаре

---
+++ task55/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+Задайте двумерный массив. 
+Напишите программу, которая заменяет строки на столбцы. 
+В случае, если это невозможно, программа должна 
+вывести сообщение для пользователя.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+В итоге получается вот такой массив:
+1 5 8 5
+4 9 4 2
+7 2 2 6
+2 3 4 7
+*/
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i,j] = new Random().Next(-deviation,deviation + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DArray (int[,] arrayToPrint)
+{
+    Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] RowsToColumns(int[,] array)
+{
+    int[,] matrix = new int[array.GetLength(0), array.GetLength(1)];
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        return matrix;
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            matrix[j,i] = array[i,j];
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = GetRandom2DArray(5, 5, 10);
+Print2DArray(matrix);
+Console.WriteLine();
+int[,] newMatrix = RowsToColumns(matrix);
+Print2DArray(newMatrix);
---
+++ task57/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, 
+сколько раз встречается элемент входных данных
+{ 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+
+1, 2, 3
+4, 6, 1
+2, 1, 6
+1 встречается 3 раза
+2 встречается 2 раза
+3 встречается 1 раз
+4 встречается 1 раз
+6 встречается 2 раза
+*/
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i,j] = new Random().Next(-deviation,deviation + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DArray (int[,] arrayToPrint)
+{
+    Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void CountElementMatrix(int[,] matrix, Dictionary<int,int> counter)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (counter.ContainsKey(matrix[i,j]))
+            {
+                counter[matrix[i,j]]++;
+            }
+            else
+            {
+                counter.Add(matrix[i,j], 1);
+            }
+        }    
+    }
+}   
+
+Dictionary<int, int> counter = new Dictionary<int,int>();
+int[,] matrix = GetRandom2DArray(4, 5, 10);
+Print2DArray(matrix);
+CountElementMatrix(matrix, counter);
+Console.WriteLine();
+
+foreach (int key in counter.Keys)
+{
+    Console.WriteLine($"Число {key} встречается {counter[key]} раз");
+}
